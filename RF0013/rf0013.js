1d7b7e3c78ad6ce322d50ad07088257454bd0153
Alires Rosa <alires.cardoso@ufpr.br>
2024-10-01 13:53:46 -0300

    implementado as cores dos botoes da solicitacoes

--- RF0013/rf0013.js
+++ RF0013/rf0013.js
@@ -27,13 +27,7 @@ document.addEventListener("DOMContentLoaded", function () {
         id_employee: "002"
 
       },
-      {
-        date: "2024-09-11 14:00",
-        description: "Mouse Microsoft",
-        status: "OUTRO",
-        id_employee: "002"
-
-      },
+  
       {
         date: "2024-10-01 09:00",
         description: "Mouse Logitech",
@@ -41,12 +35,23 @@ document.addEventListener("DOMContentLoaded", function () {
         id_employee: "002"
 
       },
+      {
+        date: "2024-10-01 09:00",
+        description: "Teclado Logitech",
+        status: "PAGA",
+        id_employee: "002"
+
+      },
     ];
+
+    // falta implementar funcao que filtra pela data e funcionario
+
+    // ================================
   
-    // const statusAtualizado = localStorage.getItem("statusSolicitacao");
-    // if (statusAtualizado) {
-    //   requests[0].status = statusAtualizado;
-    // }
+    const statusAtualizado = localStorage.getItem("statusSolicitacao");
+    if (statusAtualizado) {
+      requests[0].status = statusAtualizado;
+    }
   
     const tableBody = document.querySelector("#requestsTable tbody");
   
@@ -73,20 +78,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
         const approveButton = document.createElement("button");
         approveButton.textContent = "Aprovar/Rejeitar Serviço";
-        approveButton.classList.add("aprovar");
+        approveButton.classList.add("orcada");
         approveButton.addEventListener("click", function () {
           // Redireciona para a página de pagamento
           window.location.href = "../Budget/budget.html"; // Ajuste o caminho conforme necessário
         });
         actionCell.appendChild(approveButton);
   
-      } else if (request.status === "REJEITADA") {
+      } else if (request.status === "REJEITADA" ) {
 
         statusCell.classList.add("rejeitada")
 
         const resgatarButton = document.createElement("button");
         resgatarButton.textContent = "Resgatar Serviço";
-        resgatarButton.classList.add("resgatar");
+        resgatarButton.classList.add("rejeitar");
         actionCell.appendChild(resgatarButton);
         resgatarButton.addEventListener("click", function () {
           // muda status da rejeitada para aprovada
@@ -99,9 +104,10 @@ document.addEventListener("DOMContentLoaded", function () {
             statusCell.classList.add("aberta")
             const resgatarButton = document.createElement("button");
             resgatarButton.textContent = "Efetuar Orçamento";
-            resgatarButton.classList.add("resgatar");
+            resgatarButton.classList.add("aberta");
             actionCell.appendChild(resgatarButton);
             resgatarButton.addEventListener("click", function () {
+              // redirecionada para essa pagina (nao criada ainda?!)
                 window.location.href = "../RF0012/rf0012.html"; // Ajuste o caminho conforme necessário
             });
       }
@@ -111,28 +117,43 @@ document.addEventListener("DOMContentLoaded", function () {
 
         statusCell.classList.add("arrumada");
         const pagarButton = document.createElement("button");
-        pagarButton.textContent = "Pagar Serviço";
-        pagarButton.classList.add("pagar");
+        pagarButton.textContent = "Efetuar manuntencao";
+        pagarButton.classList.add("arrumada");
         pagarButton.addEventListener("click", function () {
-          // Redireciona para a página de pagamento
-          window.location.href = "../Pagamento/pagamento.html"; // Ajuste o caminho conforme necessário
+          // Redireciona para a efetuar manuntencao
+          window.location.href = "../RF0014/rf014.html"; // Ajuste o caminho conforme necessário
         });
         actionCell.appendChild(pagarButton);
       }
-// ===========APROVADA ??? PAREI AQUI FALTA IMPLEMENTAR OUTROS ESTADOS
+// ===========APROVADA 
       else if (request.status === "APROVADA") {
 
         statusCell.classList.add("aprovada");
         const Button = document.createElement("button");
         Button.textContent = "Efetuar Manuntenção";
-        // Button.classList.add("");
+        Button.classList.add("aprovada");
         Button.addEventListener("click", function () {
-          // Redireciona para a página de pagamento
+          // Redireciona para a página efetuar manuntencao
           window.location.href = "../RF0014/rf014.html"; // Ajuste o caminho conforme necessário
         });
         actionCell.appendChild(Button);
       }
     //   =========FIM ARRUMADA
+
+    // ===========PAGA
+    else if (request.status === "PAGA") {
+
+      statusCell.classList.add("paga");
+      const Button = document.createElement("button");
+      Button.textContent = "Finalizar Solicitação";
+      Button.classList.add("paga");
+      Button.addEventListener("click", function () {
+        // Redireciona para a página  finalizar manuntencao
+        window.location.href = "../RF0016/rf016.html"; // Ajuste o caminho conforme necessário
+      });
+      actionCell.appendChild(Button);
+    }
+  //   =========FIM ARRUMADA
       else {
         const visualizarButton = document.createElement("button");
         visualizarButton.textContent = "Visualizar Serviço";
