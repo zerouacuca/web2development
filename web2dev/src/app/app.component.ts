90e77120ac7e1f17103e1c867d72006e66b24f8f
Luccachl <lucca.hmcps@gmail.com>
2024-10-05 11:15:27 -0300

    pglogin

--- web2dev/src/app/app.component.ts
+++ web2dev/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { PgClienteComponent } from "./components/pg-cliente/pg-cliente.component";
+import { LoginComponent } from "./components/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, PgClienteComponent],
+  imports: [RouterOutlet, HeaderComponent, PgClienteComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
