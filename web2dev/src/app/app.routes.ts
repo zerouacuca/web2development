4bf9cbee1895fac7a51174e1082d11b2142d1304
Luccachl <lucca.hmcps@gmail.com>
2024-10-05 12:12:34 -0300

    roteamento pgcliente -> novasolicitacao, novasolicitacao -> pgcliente

--- web2dev/src/app/app.routes.ts
+++ web2dev/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { PgClienteComponent } from './components/pg-cliente/pg-cliente.component';
+import { NovaSolicitacaoComponent } from './components/nova-solicitacao/nova-solicitacao.component';
 
 export const routes: Routes = [
     {'path': 'login', component:LoginComponent},
-    {'path': 'pgcliente', component:PgClienteComponent}
+    {'path': 'pgcliente', component:PgClienteComponent},
+    {'path': 'novasolicitacao', component:NovaSolicitacaoComponent}
 ];
---
+++ web2dev/src/app/components/nova-solicitacao/nova-solicitacao.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+
+@Component({
+  selector: 'app-nova-solicitacao',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './nova-solicitacao.component.html',
+  styleUrl: './nova-solicitacao.component.css'
+})
+export class NovaSolicitacaoComponent {
+
+}
