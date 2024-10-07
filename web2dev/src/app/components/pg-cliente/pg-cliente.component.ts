af8b0cae4298264a55026469092fbf48e7fb1cc5
Luccachl <lucca.hmcps@gmail.com>
2024-10-07 14:01:11 -0300

    Logica pgcliente feita, falta fazer as paginas que ela rotea para

--- web2dev/src/app/components/pg-cliente/pg-cliente.component.ts
+++ web2dev/src/app/components/pg-cliente/pg-cliente.component.ts
@@ -1,14 +1,65 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
-import { LoginComponent } from '../login/login.component';
-
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from "../header/header.component"; 
+import { LoginComponent } from '../login/login.component'; 
+import { NgFor, NgIf, CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-pg-cliente',
   standalone: true,
-  imports: [HeaderComponent,LoginComponent],
+  imports: [HeaderComponent, LoginComponent, NgFor, NgIf, CommonModule],
   templateUrl: './pg-cliente.component.html',
-  styleUrl: './pg-cliente.component.css'
+  styleUrls: ['./pg-cliente.component.css'],
 })
-export class PgClienteComponent {
+export class PgClienteComponent implements OnInit {
+  requests = [
+    {
+      date: '2024-09-15 10:00',
+      description: 'Impressora HP LaserJet',
+      status: 'ORÇADA',
+    },
+    {
+      date: '2024-09-14 09:30',
+      description: 'Notebook Dell Inspiron',
+      status: 'APROVADA',
+    },
+    {
+      date: '2024-09-13 08:45',
+      description: 'Monitor Samsung',
+      status: 'REJEITADA',
+    },
+    {
+      date: '2024-09-12 11:15',
+      description: 'Teclado Logitech',
+      status: 'ARRUMADA',
+    },
+    {
+      date: '2024-09-11 14:00',
+      description: 'Mouse Microsoft',
+      status: 'OUTRO',
+    },
+  ];
 
+  ngOnInit() {
+    const statusAtualizado = localStorage.getItem('statusSolicitacao');
+    if (statusAtualizado) {
+      this.requests[0].status = statusAtualizado;
+      localStorage.clear();
+    }
+  }
+
+  aprovarServico() {
+    [RouterLink];
+  }
+
+  resgatarServico(index: number) {
+    this.requests[index].status = 'APROVADA';
+  }
+
+  pagarServico() {
+    [RouterLink];
+  }
+
+  visualizarServico() {
+    [RouterLink];
+  }
 }
