6d328cf4ea86fe289b04cb8b97886bedc774e7c2
zerouacuca <souzaoliveira@ufpr.br>
2024-10-09 20:53:19 -0300

    +Bootstrap +ngBootstrap

--- web2dev/src/main.ts
+++ web2dev/src/main.ts
@@ -1,3 +1,5 @@
+/// <reference types="@angular/localize" />
+
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
