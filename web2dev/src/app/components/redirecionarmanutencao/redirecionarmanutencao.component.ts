ecc4acf89f27e372126697cbbaf0d6c86d19556d
Gabriel Ferreira <gabrielbferreira53@gmail.com>
2024-10-20 20:17:05 -0300

    Adicionada a modal de envio e tratamento de erro para a opção de Enviar Redirecionamento da página Redirecionar Manutenção

--- web2dev/src/app/components/redirecionarmanutencao/redirecionarmanutencao.component.ts
+++ web2dev/src/app/components/redirecionarmanutencao/redirecionarmanutencao.component.ts
@@ -1,21 +1,39 @@
 import { Component } from '@angular/core';
 import { HeaderfuncionarioComponent } from "../headerfuncionario/headerfuncionario.component";
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-redirecionarmanutencao',
   standalone: true,
   imports: [HeaderfuncionarioComponent],
   templateUrl: './redirecionarmanutencao.component.html',
-  styleUrl: './redirecionarmanutencao.component.css'
+  styleUrls: ['./redirecionarmanutencao.component.css']
 })
 export class RedirecionarmanutencaoComponent {
   funcionarioSelecionado: string = "Selecionar";
+  modalVisivel: boolean = false; 
+  mensagemModal: string = ""; 
+
   constructor(private router: Router) {}
-  voltar(){
+
+  voltar() {
     this.router.navigate(["aplicarmanutencao"]);
   }
 
   selectOption(funcionario: string) {
     this.funcionarioSelecionado = funcionario;
+  }
+
+  mostrarModal() {
+    if (this.funcionarioSelecionado === "Selecionar" || this.funcionarioSelecionado === "Maria") {
+      this.mensagemModal = "Opção de redirecionamento de manutenção inválida. Selecione um funcionário e que não seja você mesmo";
+    } else {
+      this.mensagemModal = "Redirecionamento enviado com sucesso!";
+    }
+    this.modalVisivel = true; 
+  }
+
+  fecharModal() {
+    this.modalVisivel = false;
   }
 }
