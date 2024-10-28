269153c16db4ad7092db5f124998010bb58a8fba
Luccachl <lucca.hmcps@gmail.com>
2024-10-28 13:56:19 -0300

    Esqueleto backend

---
+++ backend/sistema-manutencao/src/main/java/br/net/manutencao/sistema_manutencao/SistemaManutencaoApplication.java
@@ -0,0 +1,13 @@
+package br.net.manutencao.sistema_manutencao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaManutencaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaManutencaoApplication.class, args);
+	}
+
+}
