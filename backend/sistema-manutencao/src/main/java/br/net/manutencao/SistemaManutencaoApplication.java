30f6479aa4176a46b015813136861ee7ba1f382a
Luccachl <lucca.hmcps@gmail.com>
2024-10-29 17:31:09 -0300

    Quase pronto, precisa de correção de bugs

--- backend/sistema-manutencao/src/main/java/br/net/manutencao/SistemaManutencaoApplication.java
+++ backend/sistema-manutencao/src/main/java/br/net/manutencao/SistemaManutencaoApplication.java
@@ -9,5 +9,4 @@ public class SistemaManutencaoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaManutencaoApplication.class, args);
 	}
-
 }
--- frontend/src/app/components/cadastro/cadastro.component.ts
+++ frontend/src/app/components/cadastro/cadastro.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CepService } from '../../services/cep.service'; 
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-cadastro',
@@ -20,7 +21,7 @@ export class CadastroComponent {
   bairro: string = '';
   localidade: string = '';
 
-  constructor(private cepService: CepService) {}
+  constructor(private cepService: CepService, private http: HttpClient) {}
 
   buscarCep() {
     this.cepService.buscarCep(this.cep).subscribe(
@@ -36,7 +37,7 @@ export class CadastroComponent {
   }
 
   onSubmit() {
-    console.log('Form submitted', {
+    const cadastroData = {
       cpf: this.cpf,
       nome: this.nome,
       email: this.email,
@@ -45,6 +46,16 @@ export class CadastroComponent {
       rua: this.rua,
       bairro: this.bairro,
       localidade: this.localidade
-    });
+    };
+
+    this.http.post('/clientes/autocadastro', cadastroData).subscribe(
+      () => {
+        alert('Cadastro realizado com sucesso! Verifique seu email para receber a senha.')
+      },
+      (error) => {
+        console.error('Erro ao cadastrar:', error);
+        alert('Erro ao cadastrar: ' + error.error.message);
+      }
+    );
   }
 }
