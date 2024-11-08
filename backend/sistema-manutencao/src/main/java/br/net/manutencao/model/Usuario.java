8c693cffaa9ae3341ef4f30de305855fc5bf65f9
Luccachl <lucca.hmcps@gmail.com>
2024-11-07 21:44:44 -0300

    Estrutura quase pronta

---
+++ backend/sistema-manutencao/src/main/java/br/net/manutencao/model/Usuario.java
@@ -0,0 +1,32 @@
+package br.net.manutencao.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Entity
+@Data
+abstract class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(unique = true, nullable = false)
+    private String cpf;
+
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String senha;
+
+    
+}
