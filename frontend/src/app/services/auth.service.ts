334c87386616276dab109a6d00129a62b17cb325
aruniamstel <bioaruni@gmail.com>
2024-11-14 06:36:37 -0300

    alteracao no serviço de login para inegracao com backend

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() {}
+  private baseUrl = 'http://localhost:8080/api/auth';
+
+  constructor(private http: HttpClient) {}
 
   login(email: string, senha: string): boolean {
     // Implementar chamada http backend depois bem aqui.
     return email === 'email' && senha === 'senha';
   }
+
+  registerUser(userData: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/register`, userData);
+  }
+  
 }
