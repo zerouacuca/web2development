1ae4baaedc399efcf60b03ecf1a5db72608f1976
Leonardo <leonardocorreia79@hotmail.com>
2024-11-13 23:31:41 -0300

    Back feito das RF 011, 012, 006 e 007, falta arrumar o redirecionamento no TS correspondente no Front

--- backend/sistema-manutencao/src/main/java/br/net/manutencao/controller/SolicitacaoController.java
+++ backend/sistema-manutencao/src/main/java/br/net/manutencao/controller/SolicitacaoController.java
@@ -1,5 +1,48 @@
 package br.net.manutencao.controller;
 
+import br.net.manutencao.model.Solicitacao;
+import br.net.manutencao.service.SolicitacaoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/solicitacoes")
 public class SolicitacaoController {
-    
+     private final SolicitacaoService solicitacaoService;
+
+    public SolicitacaoController(SolicitacaoService solicitacaoService) {
+        this.solicitacaoService = solicitacaoService;
+    }
+
+    // RF011 
+    @GetMapping("/abertas")
+    public List<Solicitacao> getSolicitacoesAbertas() {
+        return solicitacaoService.getSolicitacoesAbertas();
+    }
+
+    // RF012 
+    @PostMapping("/{id}/orcamento")
+    public Solicitacao efetuarOrcamento(
+            @PathVariable Long id,
+            @RequestParam Double valor,
+            @RequestParam String funcionario) {
+        return solicitacaoService.efetuarOrcamento(id, valor, funcionario);
+    }
+
+     
+    // RF006 
+    @PostMapping("/{id}/aprovar")
+    public Solicitacao aprovarServico(@PathVariable Long id) {
+        return solicitacaoService.aprovarServico(id);
+    }
+
+    // RF007
+    @PostMapping("/{id}/rejeitar")
+    public Solicitacao rejeitarServico(
+            @PathVariable Long id,
+            @RequestParam String motivo) {
+        return solicitacaoService.rejeitarServico(id, motivo);
+    }
+}
 }
--- backend/sistema-manutencao/src/main/java/br/net/manutencao/model/Solicitacao.java
+++ backend/sistema-manutencao/src/main/java/br/net/manutencao/model/Solicitacao.java
@@ -1,5 +1,137 @@
 package br.net.manutencao.model;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "solicitacoes")
 public class Solicitacao {
+      @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String cliente;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    private String estado; //  "ABERTA", "ORÇADA" e tals
+
     
+    private LocalDateTime dataHoraAbertura;
+    private LocalDateTime dataHoraOrcamento;
+    private LocalDateTime dataHoraAprovacao;
+    private LocalDateTime dataHoraRejeicao;
+    private Double valorOrcamento;
+
+    @Column(name = "funcionario_orcamento")
+    private String funcionarioOrcamento;
+
+      public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public LocalDateTime getDataHoraAbertura() {
+        return dataHoraAbertura;
+    }
+
+    public void setDataHoraAbertura(LocalDateTime dataHoraAbertura) {
+        this.dataHoraAbertura = dataHoraAbertura;
+    }
+
+    public LocalDateTime getDataHoraOrcamento() {
+        return dataHoraOrcamento;
+    }
+
+    public void setDataHoraOrcamento(LocalDateTime dataHoraOrcamento) {
+        this.dataHoraOrcamento = dataHoraOrcamento;
+    }
+
+    public Double getValorOrcamento() {
+        return valorOrcamento;
+    }
+
+    public void setValorOrcamento(Double valorOrcamento) {
+        this.valorOrcamento = valorOrcamento;
+    }
+
+    public String getFuncionarioOrcamento() {
+        return funcionarioOrcamento;
+    }
+
+    public void setFuncionarioOrcamento(String funcionarioOrcamento) {
+        this.funcionarioOrcamento = funcionarioOrcamento;
+    }
+
+   
+    @Override
+    public String toString() {
+        return "Solicitacao{" +
+                "id=" + id +
+                ", cliente='" + cliente + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", estado='" + estado + '\'' +
+                ", dataHoraAbertura=" + dataHoraAbertura +
+                ", dataHoraOrcamento=" + dataHoraOrcamento +
+                ", valorOrcamento=" + valorOrcamento +
+                ", funcionarioOrcamento='" + funcionarioOrcamento + '\'' +
+                '}';
+    }
+
+    private String motivoRejeicao;
+
+     public String getMotivoRejeicao() {
+        return motivoRejeicao;
+    }
+
+    public void setMotivoRejeicao(String motivoRejeicao) {
+        this.motivoRejeicao = motivoRejeicao;
+    }
+
+    public LocalDateTime getDataHoraAprovacao() {
+        return dataHoraAprovacao;
+    }
+
+    public void setDataHoraAprovacao(LocalDateTime dataHoraAprovacao) {
+        this.dataHoraAprovacao = dataHoraAprovacao;
+    }
+
+    public LocalDateTime getDataHoraRejeicao() {
+        return dataHoraRejeicao;
+    }
+
+    public void setDataHoraRejeicao(LocalDateTime dataHoraRejeicao) {
+        this.dataHoraRejeicao = dataHoraRejeicao;
+    }
 }
+    
+
