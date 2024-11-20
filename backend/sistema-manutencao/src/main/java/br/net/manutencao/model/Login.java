73c4c59a93557c79a476a2feb24855c3a2d625e0
Alires Rosa <alires.cardoso@ufpr.br>
2024-11-20 08:48:47 -0300

    ads auth.guard na rota relatorioCategoria

---
+++ backend/sistema-manutencao/src/main/java/br/net/manutencao/model/Login.java
@@ -0,0 +1,25 @@
+package br.net.manutencao.model;
+
+public class Login {
+    private String login;
+    private String senha;
+
+    public Login(){}
+    public Login(String login, String senha){
+        this.login = login;
+        this.senha = senha;
+    }
+    public String getLogin() {
+        return login;
+    }
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
