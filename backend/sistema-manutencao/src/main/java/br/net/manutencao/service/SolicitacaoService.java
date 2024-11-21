8bea8d143eb57450dc580b4f3eb0bc2bf6ca01ce
Leonardo <leonardocorreia79@hotmail.com>
2024-11-21 15:00:59 -0300

    arrumado

--- backend/sistema-manutencao/src/main/java/br/net/manutencao/service/SolicitacaoService.java
+++ backend/sistema-manutencao/src/main/java/br/net/manutencao/service/SolicitacaoService.java
@@ -19,6 +19,58 @@ public class SolicitacaoService {
     public List<Solicitacao> buscarPorCliente(Long idCliente) {
         return solicitacaoRepository.findByClienteId(idCliente);
     }
+      public Solicitacao aprovarServico(Long id) {
+        Solicitacao solicitacao = solicitacaoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Solicitação não encontrada"));
+
+        solicitacao.setEstado("APROVADA");
+        solicitacao.setDataHoraAprovacao(LocalDateTime.now());
+
+        return solicitacaoRepository.save(solicitacao);
+    }
+
+    public Solicitacao rejeitarServico(Long id, String motivo) {
+        Solicitacao solicitacao = solicitacaoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Solicitação não encontrada"));
+
+        solicitacao.setEstado("REJEITADA");
+        solicitacao.setMotivoRejeicao(motivo);
+        solicitacao.setDataHoraRejeicao(LocalDateTime.now());
+
+        return solicitacaoRepository.save(solicitacao);
+    }
     
+    public List<Solicitacao> getSolicitacoesAbertas() {
+        return solicitacaoRepository.findByEstado("ABERTA");
+    }
+
+    public Solicitacao efetuarOrcamento(Long id, Double valorOrcamento, String funcionario) {
+        Solicitacao solicitacao = solicitacaoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Solicitação não encontrada"));
+
+        solicitacao.setValorOrcamento(valorOrcamento);
+        solicitacao.setFuncionarioOrcamento(funcionario);
+        solicitacao.setDataHoraOrcamento(LocalDateTime.now());
+        solicitacao.setEstado("ORÇADA");
+
+        return solicitacaoRepository.save(solicitacao);
+    }
+
+    public List<Solicitacao> listarSolicitacoesFiltradas(
+            String estado, LocalDateTime inicio, LocalDateTime fim, String destino) {
+        return solicitacaoRepository.findSolicitacoesFiltradas(estado, inicio, fim, destino);
+    }
+
+    public Solicitacao finalizarSolicitacao(Long id, String funcionario) {
+        Solicitacao solicitacao = solicitacaoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Solicitação não encontrada"));
+
+        solicitacao.setEstado("FINALIZADA");
+        solicitacao.setDataHoraFinalizacao(LocalDateTime.now());
+        solicitacao.setFuncionarioFinalizacao(funcionario);
+
+        return solicitacaoRepository.save(solicitacao);
+    }
 }
+
 
