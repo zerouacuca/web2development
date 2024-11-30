85508cdf315823ecdb43152baacff86903f0ba3d
zerouacuca <souzaoliveira@ufpr.br>
2024-11-30 16:18:00 -0300

    feat: CRUD funcionario backend

---
+++ backend/sistema-manutencao/src/main/java/br/net/manutencao/DTO/FuncionarioListDTO.java
@@ -0,0 +1,16 @@
+package br.net.manutencao.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FuncionarioListDTO {
+
+    private String email;
+    private String nome;
+    private String dataNasc;
+
+}
