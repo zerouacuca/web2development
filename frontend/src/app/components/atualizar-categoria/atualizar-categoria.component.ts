466c2cba33fa4cfa4500175a4a9b6cdf71c4e132
Luccachl <lucca.hmcps@gmail.com>
2024-12-02 19:53:36 -0300

    pequeno bug corrigido

--- frontend/src/app/components/atualizar-categoria/atualizar-categoria.component.ts
+++ frontend/src/app/components/atualizar-categoria/atualizar-categoria.component.ts
@@ -21,14 +21,18 @@ export class AtualizarCategoriaComponent {
     private router: Router
   ){}
 
-  atualizar() : void{
-    if(this.formCategoria.form.valid){
-      this.categoriaService.atualizar(this.categoria);
-      this.router.navigate(["/categorias/listar"])
+  atualizar(): void {
+    if (this.categoria.id) {
+      // Passar o id e o objeto categoria para o serviço
+      this.categoriaService.atualizar(this.categoria.id, this.categoria).subscribe(
+        response => {
+          console.log('Categoria atualizada com sucesso:', response);
+          // Aqui você pode redirecionar o usuário ou mostrar uma mensagem de sucesso
+        },
+        error => {
+          console.error('Erro ao atualizar categoria:', error);
+        }
+      );
     }
-  }
-
-  listarTodos(): Categoria[]{
-    return this.categoriaService.listarTodos();
   }
 }
